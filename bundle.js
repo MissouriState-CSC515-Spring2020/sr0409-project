990fd097a7e7b28f5ba4fdcde9f2a9f40bf09761
Razumov <sr0409@live.missouristate.edu>
2020-01-31 16:16:42 -0600

    Edited gitignore and removed extra bundle.js

--- bundle.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-
-  let newDiv = document.getElementsByTagName("h1");
-  newDiv.innerHTML("Hi");
-  newDiv.appendChild(newContent); 
-
-
-}());
--- src/bundle.js
+++
@@ -1,8 +0,0 @@
-(function () {
-	'use strict';
-
-	let h1 = document.createElement("h1");
-	h1.innerHTML = "Hello World!";
-	document.body.appendChild(h1);
-
-}());
