a6b5df1e87a0b94462699ca124f2c7a8a4abccf8
User <sr0876392@otc.edu>
2020-02-07 00:57:40 -0600

    Created webpages, styled, added content

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
-let h1 = document.createElement("h1");
-h1.innerHTML = "Hello World!";
-document.body.appendChild(h1);
+function getElement(element)  {
+    let elementSrc = element.src;
+    return elementSrc;
+   
+}
+
