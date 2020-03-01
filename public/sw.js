0f72d0acaf8aed6f7454228f5069b555146b7a10
sr0409 <sr0409@live.missouristate.edu>
2020-03-01 16:01:20 -0600

    Minor changes to the SW (non-functional)

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 console.log("SW Started");
-var CACHE_NAME = 'cache';
+const CACHE_NAME = 'ourcache';
   const OFFLINE_URL = 'offline.html';
-  var urlsToCache = [
+  let urlsToCache = [
     'manifest.json',
     'offline.html',
     'index.html',
@@ -11,14 +11,11 @@ var CACHE_NAME = 'cache';
 
   self.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          // Open a cache and cache our files
-          return cache.addAll(urlsToCache);
-        })
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
     );
   });
-
   self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.match(event.request)
